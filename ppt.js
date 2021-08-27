27a66c2c92bc2f83f23f8f0d49e44ddb3f7e5f4e
ras8928 <tmp.rashid@gmail.com>
2021-08-28 03:45:57 +0500

    refactor ppt

--- ppt.js
+++ ppt.js
@@ -12,7 +12,7 @@ const puppeteer = require('puppeteer');
  * @param {Boolean} debug
  * @return {{page:puppeteer.Page,browser:puppeteer.Browser}}
  */
-async function getBrowser(debug) {
+module.exports.getBrowser = async debug => {
   if (debug) {
     const browser = await puppeteer.connect({
       browserURL: 'http://localhost:21222',
@@ -38,5 +38,67 @@ async function getBrowser(debug) {
 
     return { browser, page };
   }
-}
-exports.getBrowser = getBrowser;
+};
+
+/** @param {puppeteer.Page} page */
+module.exports.getIframe = async (page, frameSrc) => {
+  const elementHandle = await page.$('[src="' + frameSrc + '"]');
+  const frame = await elementHandle.contentFrame();
+  return frame;
+};
+
+/** @param {puppeteer.Page} page */
+module.exports.screenshot = async (page, filename = 'example.png') => {
+  await page.screenshot({ path: filename });
+};
+
+/** @param {puppeteer.Page} page */
+module.exports.setValue = async (page, selector_name, value_name) => {
+  await page.waitForSelector(selector_name);
+
+  const params = { selector_name, value_name };
+
+  await page.evaluate(p => {
+    document.querySelector(p.selector_name).value = p.value_name;
+  }, params);
+};
+
+/** @param {puppeteer.Page} page */
+module.exports.type = async (page, selector_name, value_name) => {
+  await page.waitForSelector(selector_name);
+
+  await page.focus(selector_name);
+  await click(page, selector_name);
+  await page.keyboard.type(value_name, { delay: 1 });
+  // await page.type(selector_name, value_name, { delay: 1 });
+};
+
+/** @param {puppeteer.Page} page */
+module.exports.getText = async (
+  page,
+  parentEl,
+  selector,
+  attr = 'innerText',
+) => {
+  await page.waitForSelector(selector);
+
+  let element = await parentEl.$(selector);
+  const text = await page.evaluate(
+    (element, attr) => element[attr],
+    element,
+    attr,
+  );
+  return text.trim();
+};
+
+module.exports.click = async (page, selector_name) => {
+  await page.waitForSelector(selector_name);
+  await page.click(selector_name);
+};
+
+/** @param {puppeteer.Page} page */
+module.exports.goto = async (page, url) => {
+  await page.goto(url, {
+    waitUntil: 'networkidle0',
+  });
+};
